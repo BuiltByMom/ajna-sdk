1bd58943c81ac9e10b2b2240cab0252e4de7f18a
xdemocle <xdemocle@gmail.com>
2022-10-21 16:26:31 +0200

    refactoring

--- src/connectors/meta-mask.ts
+++ src/connectors/meta-mask.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import detectEthereumProvider from '@metamask/detect-provider';
 import { MetaMaskInpageProvider } from '@metamask/providers';
