53f52e9c6687f562e052c3bc9f5de8b22930394a
Isaac Kang <isaac@ajna.finance>
2023-06-09 13:31:00 -0400

    add type-chain types

--- src/types/type-chain.ts
+++ src/types/type-chain.ts
@@ -15,6 +15,7 @@ import {
   RewardsManager__factory,
   ERC20PoolFactory__factory,
   ERC721PoolFactory__factory,
+  ERC20Pool__factory,
 } from './contracts';
 import { ERC20Interface } from './contracts/ERC20';
 import { ERC20PoolInterface } from './contracts/ERC20Pool';
@@ -25,33 +26,32 @@ import { PositionManagerInterface } from './contracts/PositionManager';
 import { RewardsManagerInterface } from './contracts/RewardsManager';
 
 export type TOKEN_CONTRACT = ERC20 | ERC721;
-export type TOKEN_FACTORY_CONTRACT = ERC20__factory | ERC721__factory;
-export type TOKEN_POOL_CONTRACT = ERC20Pool | ERC721Pool;
-export type POOL_CONTRACT = TOKEN_POOL_CONTRACT | PoolInfoUtils;
-export type POOL_FACTORY_CONTRACT = ERC20PoolFactory | ERC721PoolFactory | PoolInfoUtils__factory;
+export type TOKEN_POOL = ERC20Pool | ERC721Pool;
+export type POOL_CONTRACT = TOKEN_POOL | PoolInfoUtils;
+export type TOKEN_POOL_FACTORY = ERC20PoolFactory | ERC721PoolFactory;
+export type TOKEN_POOL_CONTRACTS = TOKEN_POOL | TOKEN_POOL_FACTORY;
 
-export type TOKEN_CONTRACTS = TOKEN_CONTRACT | POOL_FACTORY_CONTRACT;
-export type POOL_CONTRACTS = POOL_CONTRACT | POOL_FACTORY_CONTRACT;
+// export type TOKEN_CONTRACTS = TOKEN_CONTRACT | POOL_FACTORY_CONTRACT;
+// export type POOL_CONTRACTS = POOL_CONTRACT | POOL_FACTORY_CONTRACT;
 
-export type FACTORY_CONTRACTS =
-  | POOL_FACTORY_CONTRACT
-  | TOKEN_FACTORY_CONTRACT
-  | ERC20PoolFactory__factory
-  | ERC721PoolFactory__factory
-  | PoolInfoUtils__factory
-  | PositionManager__factory
-  | RewardsManager__factory;
-export type OTHER_CONTRACTS = PositionManager | RewardsManager;
+// export type FACTORY_CONTRACTS =
+//   | POOL_FACTORY_CONTRACT
+//   | ERC20PoolFactory__factory
+//   | ERC721PoolFactory__factory
+//   | PoolInfoUtils__factory
+//   | PositionManager__factory
+//   | RewardsManager__factory;
+// export type OTHER_CONTRACTS = PositionManager | RewardsManager;
 
-export type AJNA_CONTRACTS = TOKEN_CONTRACTS | POOL_CONTRACTS | FACTORY_CONTRACTS | OTHER_CONTRACTS;
+// export type AJNA_CONTRACTS = TOKEN_CONTRACTS | POOL_CONTRACTS | FACTORY_CONTRACTS | OTHER_CONTRACTS;
 
-export type TOKEN_CONTRACT_IFACE = ERC20Interface | ERC721Interface;
-export type POOL_CONTRACT_IFACE = ERC20PoolInterface | ERC721PoolInterface;
+// export type TOKEN_CONTRACT_IFACE = ERC20Interface | ERC721Interface;
+// export type POOL_CONTRACT_IFACE = ERC20PoolInterface | ERC721PoolInterface;
 
-export type OTHER_CONTRACTS_IFACES = PositionManagerInterface | RewardsManagerInterface;
-export type TOKEN_POOL_CONTRACT_IFACE =
-  | TOKEN_CONTRACT_IFACE
-  | POOL_CONTRACT_IFACE
-  | PoolInfoUtilsInterface;
+// export type OTHER_CONTRACTS_IFACES = PositionManagerInterface | RewardsManagerInterface;
+// export type TOKEN_POOL_CONTRACT_IFACE =
+//   | TOKEN_CONTRACT_IFACE
+//   | POOL_CONTRACT_IFACE
+//   | PoolInfoUtilsInterface;
 
-export type ALL_IFACES = TOKEN_POOL_CONTRACT_IFACE | OTHER_CONTRACTS_IFACES;
+// export type ALL_IFACES = TOKEN_POOL_CONTRACT_IFACE | OTHER_CONTRACTS_IFACES;
