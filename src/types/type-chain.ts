bc0c9e84d6b128124d6595b31ba7e5e5af333d61
Isaac Kang <isaac@ajna.finance>
2023-06-13 20:02:55 -0400

    add custom contract types

--- src/types/type-chain.ts
+++ src/types/type-chain.ts
@@ -1,3 +1,4 @@
+import { BigNumber, PopulatedTransaction } from 'ethers';
 import {
   ERC20,
   ERC20Pool,
@@ -19,16 +20,36 @@ import { PoolInfoUtilsInterface } from './contracts/PoolInfoUtils';
 import { PositionManagerInterface } from './contracts/PositionManager';
 import { RewardsManagerInterface } from './contracts/RewardsManager';
 
+export interface CustomContractTypes {
+  estimateGas: {
+    [key: string]: (...args: any[]) => Promise<BigNumber>;
+  };
+  populateTransaction: {
+    [key: string]: (...args: any[]) => Promise<PopulatedTransaction>;
+  };
+  callStatic: {
+    [key: string]: (...args: any[]) => Promise<any>;
+  };
+  functions: {
+    [key: string]: (...args: any[]) => Promise<any>;
+  };
+  [key: string]: any;
+}
+
 // Contracts
-export type TOKEN_CONTRACT = ERC20 | ERC721;
-export type TOKEN_POOL = ERC20Pool | ERC721Pool;
-export type POOL_FACTORY = ERC20PoolFactory | ERC721PoolFactory;
+export type TOKEN_CONTRACT = (ERC20 | ERC721) & CustomContractTypes;
+export type TOKEN_POOL = (ERC20Pool | ERC721Pool) & CustomContractTypes;
+export type POOL_FACTORY = (ERC20PoolFactory | ERC721PoolFactory) & CustomContractTypes;
 export type POOL_CONTRACT = TOKEN_POOL | POOL_FACTORY;
 export type TOKEN_POOL_CONTRACT = TOKEN_CONTRACT | POOL_CONTRACT;
+export type POOL_CONTRACT_AND_UTILS = POOL_FACTORY | (PoolInfoUtils & CustomContractTypes);
 
-export type MANAGER_CONTRACT = PositionManager | RewardsManager;
+export type MANAGER_CONTRACT = (PositionManager | RewardsManager) & CustomContractTypes;
 
-export type ALL_CONTRACTS = TOKEN_POOL_CONTRACT | MANAGER_CONTRACT | PoolInfoUtils;
+export type ALL_CONTRACTS =
+  | TOKEN_POOL_CONTRACT
+  | MANAGER_CONTRACT
+  | (PoolInfoUtils & CustomContractTypes);
 
 // Interfaces
 export type TOKEN_CONTRACT_IFACE = ERC20Interface | ERC721Interface;
