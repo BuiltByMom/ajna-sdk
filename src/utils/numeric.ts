d564132cc79dd787db812bddd756ba0b5e43ac82
Isaac Kang <isaac@ajna.finance>
2023-06-21 07:15:50 -0400

    rm extraneous code

--- src/utils/numeric.ts
+++ src/utils/numeric.ts
@@ -1,6 +1,4 @@
 import { BigNumber, BigNumberish, constants, ethers } from 'ethers';
-import { formatUnits } from 'ethers/lib/utils';
-import { isBoolean } from 'mathjs';
 
 // converts from WAD precision to human-readable string
 export const fromWad = (value: BigNumberish): string => {
@@ -27,18 +25,3 @@ export const min = (lhs: BigNumber, rhs: BigNumber): BigNumber => (lhs.lte(rhs) 
 
 // returns the maximum of two WADs
 export const max = (lhs: BigNumber, rhs: BigNumber): BigNumber => (lhs.gte(rhs) ? lhs : rhs);
-
-export function formatArgValues(obj: any) {
-  return Object.keys(obj).reduce((acc, value) => {
-    if (BigNumber.isBigNumber(obj[value]) || isBoolean(obj[value])) {
-      return {
-        ...acc,
-        [value]: formatUnits(obj[value].toString(), 18),
-      };
-    }
-    return {
-      ...acc,
-      [value]: obj[value],
-    };
-  }, {});
-}
