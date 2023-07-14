2a3f396ed1e8c66837cb034c794ba5abd33de5be
Jesus Gomez <jgomez@vascarsolutions.com>
2023-07-14 10:13:03 -0500

    add tests

--- src/tests/grant-fund.spec.ts
+++ src/tests/grant-fund.spec.ts
@@ -2,7 +2,7 @@ import { expect } from '@jest/globals';
 import dotenv from 'dotenv';
 import { providers } from 'ethers';
 import { AjnaSDK } from '../classes/AjnaSDK';
-import { startNewDistributionPeriod } from '../contracts/grant-fund';
+import { getProposalIdFromReceipt, startNewDistributionPeriod } from '../contracts/grant-fund';
 import { DistributionPeriod } from '../types/classes';
 import { addAccountFromKey } from '../utils/add-account';
 import { mine, timeJump } from '../utils/ganache';
@@ -20,6 +20,7 @@ const SIGNER_KEY = '0x2bbf23876aee0b3acd1502986da13a0f714c143fcc8ede8e2821782d75
 const DELEGATEE_ADDRESS = '0x8596d963e0DEBCa873A56FbDd2C9d119Aa0eB443';
 const VOTER_ADDRESS = '0xeeDC2EE00730314b7d7ddBf7d19e81FB7E5176CA';
 const VOTER_KEY = '0xd332a346e8211513373b7ddcf94b2b513b934b901258a9465c76d0d9a2b676d8';
+const PROPOSAL_TO_ADDRESS = '0xbC33716Bb8Dc2943C0dFFdE1F0A1d2D66F33515E';
 
 describe('Grants fund', () => {
   const provider = new providers.JsonRpcProvider(config.ETH_RPC_URL);
@@ -72,6 +73,30 @@ describe('Grants fund', () => {
       const tx = await startNewDistributionPeriod(signer);
       await submitAndVerifyTransaction(tx);
       await expect(ajna.distributionPeriods.getActiveDistributionPeriod()).resolves.toBeDefined();
+    });
+  });
+  describe('Proposals', () => {
+    it(`creates a new proposal`, async () => {
+      const tx = await ajna.distributionPeriods.createProposal(signer, {
+        title: 'ajna proposal test',
+        recipientAddresses: [
+          {
+            address: PROPOSAL_TO_ADDRESS,
+            amount: '1000.00',
+          },
+        ],
+        externalLink: 'https://example.com',
+        ipfsHash: '000000001',
+        arweaveTxid: '000000001',
+      });
+      const receipt = await submitAndVerifyTransaction(tx);
+      const proposalId = getProposalIdFromReceipt(receipt);
+      const proposal = ajna.distributionPeriods.getProposal(proposalId);
+      const proposalInfo = await proposal.getInfo();
+      expect(proposalInfo.votesReceived.isZero()).toBe(true);
+      expect(fromWad(proposalInfo.tokensRequested)).toBe('1000.0');
+      expect(proposalInfo.fundingVotesReceived.isZero()).toBe(true);
+      expect(proposalInfo.executed).toBe(false);
     });
   });
   describe('Delegates', () => {
